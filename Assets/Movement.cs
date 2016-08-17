aa3dc6171b9772463d5ef9fecce2d9a830b7fc20
Daniel Kiedrowski <daniel@levire.com>
2016-08-17 21:52:38 +0200

    Added a basic movement script.

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (CapsuleCollider))]
+
+public class Movement : MonoBehaviour {
+
+	Rigidbody rb;
+	public float force = 10.0f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		rb.AddForce (Input.GetAxis("Horizontal")*force, 0, Input.GetAxis("Vertical")*force);
+	}
+}
