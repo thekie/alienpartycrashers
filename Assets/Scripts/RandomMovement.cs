7ecb4b65b6976d86c43b44593951b9bb124ea5fd
Pavlo Shelyazhenko <pavlo.shelyazhenko@gmail.com>
2016-08-17 22:04:22 +0300

    Test setup for randomly moving actors.

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMovement : MonoBehaviour {
+	private float action;
+	private bool moving;
+	private Rigidbody body;
+
+	void Start () {
+		action = Time.time;
+		moving = false;
+		body = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		float current = Time.time;
+		if (current > action) {
+			if (moving) {
+				body.AddRelativeForce (new Vector3 (200, 0, 0));
+			} else {
+				body.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+			}
+			action = current + 1.0f;
+			moving = !moving;
+		}
+	}
+}
