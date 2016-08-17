f65b139918a0c6d753a73aa49db34db44544199b
Daniel Kiedrowski <daniel@levire.com>
2016-08-17 22:45:06 +0200

    A component to color a mesh, when the funk starts.

---
+++ Assets/FunkyBlink.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+
+public class FunkyBlink : MonoBehaviour {
+
+	MeshRenderer meshRenderer;
+	Color originalColor;
+
+	public Color funkyColor = Color.red;
+
+	void Start()
+	{
+		meshRenderer = gameObject.GetComponent<MeshRenderer> ();
+		originalColor = meshRenderer.material.color;
+	}
+
+	void OnEnable()
+	{
+		FunkyControl.OnFunkStarted += OnFunkStarted;
+		FunkyControl.OnFunkStopped += OnFunkStopped;
+	}
+
+
+	void OnDisable()
+	{
+		FunkyControl.OnFunkStarted -= OnFunkStarted;
+	}
+		
+	void OnFunkStarted()
+	{
+		meshRenderer.material.color = funkyColor;
+	}
+
+	void OnFunkStopped()
+	{
+		meshRenderer.material.color = originalColor;
+	}
+}
