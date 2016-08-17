4c8e09b9c250658045dbf915c695e44619f713f7
Daniel Kiedrowski <daniel@levire.com>
2016-08-17 22:44:37 +0200

    Control component which sends a message when funk starts and ends.

---
+++ Assets/FunkyControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunkyControl : MonoBehaviour {
+
+	public delegate void FunkAction();
+	public static event FunkAction OnFunkStarted;
+	public static event FunkAction OnFunkStopped;
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && OnFunkStarted != null) {
+			OnFunkStarted ();
+		}
+
+		if (Input.GetButtonUp ("Fire1") && OnFunkStopped != null) {
+			OnFunkStopped ();
+		}
+	}
+}
