d3987fcb4a063ab946dcd3e1f7554c3a13d96ab4
Pavlo Shelyazhenko <pavlo.shelyazhenko@gmail.com>
2016-08-18 09:21:06 +0300

    Initial position in circles.

--- Assets/Scripts/ActorsController.cs
+++ Assets/Scripts/ActorsController.cs
@@ -7,9 +7,11 @@ public class ActorsController : MonoBehaviour {
 	public Transform parent;
 
 	void Start () {
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 30; i++) {
+			Vector3 pos = Vector3.forward * 15f * (0.5f + (i >= 15 ? 0.5f : 0f));
+			pos = Quaternion.AngleAxis (360 * (i % 15) / 15f, Vector3.up) * pos;
 			GameObject go = Instantiate (actor, parent) as GameObject;
-			go.transform.localPosition = new Vector3 ((i % 5 - 3) * 4, 0, 4 * (i / 5));
+			go.transform.localPosition = pos;
 		}
 	}
 }
