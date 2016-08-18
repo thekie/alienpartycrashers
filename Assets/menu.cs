af2d159dffacb028b5694af3210bc21e12912e68
pinguin999 <pinguin999@gmail.com>
2016-08-18 22:43:15 +0200

    Menu

---
+++ Assets/menu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void loadmain()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
