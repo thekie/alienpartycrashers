359d500d430ce4778f5303a0f081c06ab8fcf28b
Pavlo Shelyazhenko <pavlo.shelyazhenko@gmail.com>
2016-08-18 09:06:05 +0300

    Use gravity for movers.

--- Assets/Scripts/CenterMovement.cs
+++ Assets/Scripts/CenterMovement.cs
@@ -11,33 +11,44 @@ public class CenterMovement : MonoBehaviour {
 	private Vector3 center;
 	private Stage stage;
 	private float start;
-	private float waitTime = 2.0f;
+	public float waitCenter = 1.0f;
+	public float waitTangent = 1.0f;
 	public float centerForce = 25f;
 	public float tangentForce = 10f;
+	public bool notifyStages = false;
+	private Vector3 force;
 
 	void Start () {
 		body = GetComponent<Rigidbody> ();
 		center = transform.position + Vector3.forward * 0.1f;
 		stage = Stage.Center;
 		start = Time.time;
+		force = Vector3.zero;
 	}
 
 	void FixedUpdate () {
 		switch (stage) {
 		case Stage.Center:
-			if (Time.time - start > waitTime) {
-				body.AddForce ((center - transform.position).normalized * centerForce * (Random.value / 2f + 0.5f));
+			if (Time.time - start > waitTangent) {
+				force = (center - transform.position).normalized * centerForce * (Random.value / 2f + 0.5f);
 				stage = Stage.Tangent;
 				start = Time.time;
+				if (notifyStages) {
+					Debug.Log (stage);
+				}
 			}
 			break;
 		case Stage.Tangent:
-			if (Time.time - start > waitTime) {
-				body.AddForce (Vector3.Cross (center - transform.position, Vector3.up).normalized * tangentForce * (Random.value - 1f));
+			if (Time.time - start > waitCenter) {
+				force = Vector3.Cross (center - transform.position, Vector3.up).normalized * tangentForce * (Random.value - 0.5f);
 				stage = Stage.Center;
 				start = Time.time;
+				if (notifyStages) {
+					Debug.Log (stage);
+				}
 			}
 			break;
 		}
+		body.AddForce (force);
 	}
 }
