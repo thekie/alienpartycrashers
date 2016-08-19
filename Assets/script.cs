10ab3553bc5616872dc3d1509977573e72995492
pinguin999 <pinguin999@gmail.com>
2016-08-19 13:08:26 +0200

    splash screen

---
+++ Assets/script.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class script : MonoBehaviour {
+
+    public float timeRemaining = 4f;
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("decreaseTimeRemaining", 1.0f, 1.0f);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+        if (timeRemaining == 0)
+        {
+            SceneManager.LoadScene("Scenes/menu");
+        }
+    }
+
+    void decreaseTimeRemaining()
+    {
+        timeRemaining--;
+    }
+}
