84e528f91fb1bcae4cf017eac67de64d7b155ed5
Nils Riedemann <nils@riedemann.co>
2016-08-19 11:05:52 +0200

    music fixes

---
+++ Assets/SongManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongManager : MonoBehaviour {
+	private static SongManager instance = null;
+	public static SongManager Instance {
+		get { return instance; }
+	}
+
+	[SerializeField] AudioSource WaltsSong;
+	[SerializeField] AudioSource FunkSong;
+	int funkCount = 0;
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Update() {
+		if(funkCount > 0) {
+			PlayFunk();
+		} else {
+			PlayWalts();
+		}
+	}
+
+
+	void Start () {
+		WaltsSong.Play();
+		FunkSong.Play();
+
+		FunkyControl.OnFunkStarted += (GameObject gameObject) => {
+			funkCount++;
+		};
+
+		FunkyControl.OnFunkStopped += (GameObject gameObject) => {
+			funkCount--;
+		};
+	}
+
+	void PlayFunk() {
+		Debug.Log("Play fff");
+		WaltsSong.mute = true;
+		FunkSong.mute = false;
+	}
+
+	void PlayWalts() {
+		Debug.Log("Play walt");
+		WaltsSong.mute = false;
+		FunkSong.mute = true;
+	}
+}
