ff7a5714e123b7c39aff855aa068e8f8860caf2b
Nils Riedemann <nils@riedemann.co>
2016-08-19 03:01:13 +0200

    post processing fx and everything

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+	[SerializeField] float rotationSpeed = 90f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(transform.position, transform.right, Time.deltaTime * rotationSpeed);
+	}
+}
